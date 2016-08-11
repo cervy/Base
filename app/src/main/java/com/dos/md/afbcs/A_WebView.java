7e123cb68612abeee2bfe21df7b4327f649e7064
cervy <766139427@qq.com>
2016-08-11 15:43:01 +0800

    optimized

--- app/src/main/java/com/dos/md/afbcs/A_WebView.java
+++ app/src/main/java/com/dos/md/afbcs/A_WebView.java
@@ -294,4 +294,28 @@ public class A_WebView extends A_Base {
             return;
         }
     }
+    
+    
+    public String getFromAssets(String fileName) {
+        try {
+            InputStreamReader inputReader = new InputStreamReader(
+                    getResources().getAssets().open(fileName));
+
+            BufferedReader bufReader = new BufferedReader(inputReader);
+
+            String line = "";
+            String Result = "";
+
+            while ((line = bufReader.readLine()) != null)
+                Result += line;
+            if (bufReader != null)
+                bufReader.close();
+            if (inputReader != null)
+                inputReader.close();
+            return Result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
