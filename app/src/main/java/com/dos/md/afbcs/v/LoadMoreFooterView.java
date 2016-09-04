ca69a5965b2ceb52070e2d0dc6904402290af1f0
cervy <766139427@qq.com>
2016-09-04 12:17:40 +0800

    Add files via upload

---
+++ app/src/main/java/com/dos/md/afbcs/v/LoadMoreFooterView.java
@@ -0,0 +1,60 @@
+package com.dos.md.afbcs.v;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.aspsine.swipetoloadlayout.SwipeLoadMoreTrigger;
+import com.aspsine.swipetoloadlayout.SwipeTrigger;
+
+/**
+ * Created by DOS on 2016/5/24 0024.
+ */
+public class LoadMoreFooterView extends TextView implements SwipeTrigger, SwipeLoadMoreTrigger {
+    public LoadMoreFooterView(Context context) {
+        super(context);
+    }
+
+    public LoadMoreFooterView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onLoadMore() {
+        setText("加载更多");
+    }
+
+    @Override
+    public void onPrepare() {
+        setText("");
+    }
+
+    @Override
+    public void onMove(int yScrolled, boolean isComplete, boolean automatic) {
+        if (!isComplete) {
+            if (yScrolled <= -getHeight()) {
+                setText("松手加载更多");
+            } else {
+                setText("上拉加载更多");
+            }
+
+        } else {
+            setText("LOAD MORE RETURNING");// TODO: 2016/5/24 0024
+        }
+    }
+
+    @Override
+    public void onRelease() {
+        setText("LOADING 加载更多");
+    }
+
+    @Override
+    public void onComplete() {
+        setText("加载完成");
+    }
+
+    @Override
+    public void onReset() {
+        setText("");
+    }
+}
