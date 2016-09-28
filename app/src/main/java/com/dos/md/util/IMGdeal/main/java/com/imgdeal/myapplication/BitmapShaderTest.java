1f6f2100b1e7ac4e4a608a950940e32e46af4ba1
cervy <766139427@qq.com>
2016-09-28 23:05:43 +0800

    Update BitmapShaderTest.java

--- app/src/main/java/com/dos/md/util/IMGdeal/main/java/com/imgdeal/myapplication/BitmapShaderTest.java
+++ app/src/main/java/com/dos/md/util/IMGdeal/main/java/com/imgdeal/myapplication/BitmapShaderTest.java
@@ -1,12 +1,12 @@
-package com.imooc.myimooc;
+package com.imgdeal.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * Created by Administrator on 2015/3/14 0014.
+ *  
  */
-public class BitmapShaderTest extends Activity {
+public class BitmapShader extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
