b760284502e7a0c5a0e9cb95dfeae1f04d789b7f
fgj <766139427@qq.com>
2017-08-17 12:28:56 +0800

    Update U_Validate.java

--- app/src/main/java/com/dos/md/util/U_Validate.java
+++ app/src/main/java/com/dos/md/util/U_Validate.java
@@ -477,4 +477,27 @@ public static void setViewBackgroundDrawable(View view, Drawable drawable) {
 
 }*/
 
+
+
+
+ /* 利用只读属性动画+WindowManager设置窗口透明度
+  *@param from\>=0&&from\<=1.0f
+  * @param to\>=0&&to\<=1.0f
+  * 
+  * */
+ private void dimBackground(final float from, final float to) {
+ final Window window = getWindow();
+ ValueAnimator valueAnimator = ValueAnimator.ofFloat(from, to);
+ valueAnimator.setDuration(500);
+ valueAnimator.addUpdateListener(new AnimatorUpdateListener() {
+ @Override
+ public void onAnimationUpdate(ValueAnimator animation) {
+ WindowManager.LayoutParams params = window.getAttributes();
+ params.alpha = (Float) animation.getAnimatedValue();
+ window.setAttributes(params);
+ }
+ });
+
+ valueAnimator.start();
+ }
 }
